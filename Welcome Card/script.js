757b6be9a196dc04a294a2362605bad5a5a900ef
abythomas300 <abythomas300@gmail.com>
2025-06-05 18:42:48 +0530

    Welcome Card App: initial commit

---
+++ Welcome Card/script.js
@@ -0,0 +1,9 @@
+let welcomeEl = document.getElementById("welcome-el");
+
+let userName = "Aron Taylon Jhonson";
+let greeting = "Welcome back! ";
+let message = greeting+userName;
+
+welcomeEl.innerText = message;
+
+welcomeEl.innerText += "👋"
