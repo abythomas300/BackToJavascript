fadf8612de5f52d3377d062e8e646ec1becc80dd
abythomas300 <abythomas300@gmail.com>
2025-06-05 18:41:59 +0530

    variables practice 4 problems

---
+++ variables-practice.js
@@ -0,0 +1,46 @@
+//#1
+// let firstName = "Joshwa";
+// let lastName = "Aron";
+// let fullName = firstName + " " + lastName;
+// console.log(fullName);
+
+
+
+//#2 String concatenation
+// let userName = "Linda";
+// let greeting = "Hi there";
+// function greet() {
+//     console.log(greeting + ", " + userName + "!");
+// }
+// greet();
+
+
+
+//#3 Incrementing and decrementing
+// let myPoints = 3;
+
+// function add3Points() {
+//     myPoints += 3;
+// }
+
+// function remove1Point() {
+//     myPoints -= 1;
+// }
+
+// add3Points();
+// add3Points();
+// add3Points();
+// remove1Point();
+// remove1Point();
+
+// console.log("Points after every alteration:", myPoints)
+
+
+
+//#4 Strings and numbers
+console.log("2" + 2) // 22
+console.log(11 + 7) //18
+console.log(6 + "5") // 65
+console.log("My points: " + 5 + 9) // My points: 59
+console.log(2 + 2) // 4
+console.log("11" + "14") //  1114
