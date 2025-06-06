e27fb7903e4f5c3c94096c9ea342649f28b44755
abythomas300 <abythomas300@gmail.com>
2025-06-06 12:23:59 +0530

    Basic Calculator: initial commit

--- Passenger Counter App/script.js
+++ Passenger Counter App/script.js
@@ -1,19 +1,21 @@
 let countEl = document.getElementById("count-el");
 let saveEl = document.getElementById("saveEl");
 
-let count = 0;
+let count = 0; //initializing count
 
-function incrementCount() {
-  count += 1;     
-  countEl.innerText = count;
+function incrementCount() {  
+  count += 1;                  //incrementing value of count by 1 on each click.
+  countEl.textContent = count; //reflecting the value of count on screen.
 }
 
 let countMemory = 'Saved History: ';
-let trimmedHistory = '';
+let trimmedHistory = '';       // to store hiphen removed text
 
 function save() {
   countMemory = countMemory+count+" - ";
-  let trimmedHistory = countMemory.substring(0, countMemory.length-2);  //to remove the excess hiphen
-  saveEl.innerText = trimmedHistory;
+  trimmedHistory = countMemory.substring(0, countMemory.length-2);  //to remove the excess hiphen
+  saveEl.textContent = trimmedHistory;
+  count = 0;                   //count reset
+  countEl.textContent = count; //reflecting  value of count on screen
 }
 
