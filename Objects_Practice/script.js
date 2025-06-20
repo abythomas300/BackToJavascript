72d8a14a409f4d673c1921fb56f3085e3fd4b6ed
abythomas300 <abythomas300@gmail.com>
2025-06-20 14:14:02 +0530

    Objects Practice

---
+++ Objects_Practice/script.js
@@ -0,0 +1,12 @@
+let playerName = "Aby"
+let playerScore = 79000;
+
+//Objects resembles array which is used to store elements that are connected to gether ( ELEMENTS not Values).
+
+let player = {
+     playerName : 'Aby Thomas',
+     playerScore : 79000
+};
+
+console.log(player.playerName);
+console.log(player.playerScore);
