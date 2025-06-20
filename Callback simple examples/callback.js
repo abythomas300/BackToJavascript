6a2adacf89cb362c7b1d98b1ed50b60da9f4ec9d
abythomas300 <abythomas300@gmail.com>
2025-06-20 20:21:32 +0530

    callback simple examples

---
+++ Callback simple examples/callback.js
@@ -0,0 +1,103 @@
+//CALLBACK: A function that we pass as an argument inside another function.
+
+//Example 1: Simple snippet to show normal callback working
+// function avengersAssemble(call) {
+//     console.log("The Director of Avengers is calling them.");
+//     setTimeout(()=>{
+//         console.log("The avengers are suiting up");
+//         console.log("The Avengers are ready to go!");
+//         call();
+//     }, 5000);
+// }
+
+// function startWar() {
+//     console.log("Avengers are fighting the aliens");
+//     console.log("Avengers WON!!");
+    
+// }
+
+// //function calling
+// avengersAssemble(startWar);
+
+
+// function orderPizza(callback) { 
+//     console.log("Ordering a pizza.");
+//     console.log("Order placed.");
+//     setTimeout(()=>{
+//         console.log("Pizza delivered.");
+//         callback(); 
+//     }, 2000);
+// }
+
+// function eatPizza() {
+//     console.log("I am eating the pizza");
+// }
+
+// orderPizza(eatPizza);
+
+//Harcoding a callback function without passing it as an argument
+// function orderPizza() {
+//     console.log("Ordering a pizza.");
+//     console.log("Order placed.");
+//     setTimeout(()=>{
+//         console.log("Pizza delivered.");
+//         eatPizza();
+//     }, 2000);
+// }
+
+// function eatPizza() {
+//     console.log("I am eating the pizza");
+// }
+
+// orderPizza();
+
+
+
+//Example 1 to show difference: Passing as argument vs hardcoding callback functions
+// function diffuseBomb(callback) {
+//     console.log("Time started!");
+//     setTimeout(()=>{
+//         callback();
+//     }, 3000)
+// }
+
+// function cutRedWire() {
+//     console.log("Wrong Wire! 💥"); 
+// }
+
+// function cutGreenWire() {
+//     console.log("Bomb defused successfully! 🟩");
+// }
+
+// diffuseBomb(cutRedWire);
+// diffuseBomb(cutGreenWire);
+//Conclusion: When NOT harcoding, we can pass any function, not just a particular one. 
+
+
+//Example 2 to show difference: Passing as argument vs hardcoding callback functions
+// function saveWorld(callback) {
+//     console.log("🟢Mission Started.");
+//     setTimeout(() => {
+//         console.log("✅Mission Successfull.");
+//         callback("Iron Man", "Spider Man");  //invokes 'celebrate()'
+//     }, 5000);
+// }
+// function celebrate(hero, sidekick) {
+//     console.log(`${hero} and ${sidekick} saved the world! Anyone wants Shawarma?!`);
+// }
+
+// saveWorld(celebrate);
+
+//Example 2: but passing arguments inside the first function instead of second -> Makes it more flexible and reusable
+function saveWorld(callback, hero, sidekick) {
+    console.log("🟢Mission Started.");
+    setTimeout(() => {
+        console.log("✅Mission Successfull.");
+        callback(hero, sidekick);  //invokes 'celebrate()'
+    }, 5000);
+}
+function celebrate(hero, sidekick) {
+    console.log(`${hero} and ${sidekick} saved the world! Anyone wants Shawarma?!`);
+}
+
+saveWorld(celebrate, "Dr.Strange", "Wong");
