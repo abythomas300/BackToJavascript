a20a8b95e8fcb47d6acc498a37599850476ecbb3
abythomas300 <abythomas300@gmail.com>
2025-06-22 23:09:58 +0530

    basic validation boilerplate

---
+++ Validation/script.js
@@ -0,0 +1,10 @@
+const form = document.querySelector("#form")
+const name = document.querySelector("#name")
+const age = document.querySelector("#age")
+const email = document.querySelector("#email")
+const pass = document.querySelector("#password")
+const errorElement = document.querySelector("#error-el")
+
+let errorMessages = []; // to store error messages.
+
+
