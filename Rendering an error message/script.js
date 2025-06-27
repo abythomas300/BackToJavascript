e9ebe6af3feb7f6f16baf6da887b7e5fabb75cc4
abythomas300 <abythomas300@gmail.com>
2025-06-28 00:09:10 +0530

    throw new : simple example

---
+++ Rendering an error message/script.js
@@ -0,0 +1,6 @@
+let errorParagraph = document.getElementById("error");
+
+function displayError() {
+    errorParagraph.style.color = "red"
+    errorParagraph.textContent = "Something went wrong, please try again."
+}
---
+++ forEach Looping Practice/script.js
@@ -0,0 +1,18 @@
+//ForEach 
+//This is a built in function operated on arrays.
+//Use: Loop through each item in array and do any specific function on each item.
+//Syntax: arrayName.ForEach();
+
+
+//You have a list of your loyal customers.
+// You want to send a thank-you email to each of them.
+
+const customers = ["Iron Man", "Spider-Man", "Black Widow", "Hulk"];
+
+for(let i = 0; i < customers.length; i++) {
+    console.log(`Thank you ${customers[i]}🍕`);
+}
+
+
+
+
